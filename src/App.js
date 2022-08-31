7acd88529b17faf856d44367631a3100a6d3831d
Vinicius <vinicius.luiz.9256@gmail.com>
2022-08-31 15:09:42 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Login from './components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './components/user/User';
+import ProtectedRouter from './components/Helper/ProtectedRouter';
+import Photo from './components/Photo/Photo';
+import UserProfile from './components/user/UserProfile';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      App react
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <main className='AppBody'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRouter>
+                    <User />
+                  </ProtectedRouter>
+                }
+              />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Photo/PhotoComments.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import UserContext from '../../UserContext';
+import PhotoCommentsForms from './PhotoCommentsForms';
+import styles from './PhotoComments.module.css';
+
+const PhotoComments = (props) => {
+  const { login } = React.useContext(UserContext);
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentSection = React.useRef(null);
+
+  React.useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentSection} className={`${styles.comments} ${props.single ? styles.single : ''}`}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForms single={props.single} id={props.id} setComments={setComments} />}
+    </>
+  );
+};
+
+export default PhotoComments;
